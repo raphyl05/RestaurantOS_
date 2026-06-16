3df48b27d7d25266be15c4d4bc045291e92e5157
raphyl05 <raphyleyri@gmail.com>
2026-06-16 17:41:50 -0400

    Error de versiones arreglado

--- RestaurantOS.Web/Program.cs
+++ RestaurantOS.Web/Program.cs
@@ -1,29 +1,54 @@
+using Microsoft.EntityFrameworkCore;
+using RestaurantOS.Application.Interfaces;
+using RestaurantOS.Application.Services;
+using RestaurantOS.Domain.Interfaces;
+using RestaurantOS.Infrastructure.Data;
+using RestaurantOS.Infrastructure.Repositories;
+using RestaurantOS.Infrastructure.Security;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// MVC
 builder.Services.AddControllersWithViews();
+
+// Base de datos
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Repositorios (Domain <- Infrastructure)
+builder.Services.AddScoped<IMesaRepository, MesaRepository>();
+builder.Services.AddScoped<IProductoRepository, ProductoRepository>();
+builder.Services.AddScoped<IPedidoRepository, PedidoRepository>();
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+builder.Services.AddScoped<IPagoRepository, PagoRepository>();
+
+// Seguridad
+builder.Services.AddScoped<IPasswordHasher, BcryptPasswordHasher>();
+
+// Servicios de aplicación (Application <- Application)
+builder.Services.AddScoped<IMesaService, MesaService>();
+builder.Services.AddScoped<IProductoService, ProductoService>();
+builder.Services.AddScoped<IPedidoService, PedidoService>();
+builder.Services.AddScoped<IUsuarioService, UsuarioService>();
+builder.Services.AddScoped<IPagoService, PagoService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
